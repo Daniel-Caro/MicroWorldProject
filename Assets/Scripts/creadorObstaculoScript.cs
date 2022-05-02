25dcc3abbe6882f16ea29eebc84220b4e2c4f6d6
davbarsal1 <72515663+davbarsal1@users.noreply.github.com>
2022-05-02 18:36:21 +0200

    Generacion de monedas

--- Assets/Scripts/creadorObstaculoScript.cs
+++ Assets/Scripts/creadorObstaculoScript.cs
@@ -12,7 +12,7 @@ public class creadorObstaculoScript : MonoBehaviour
     public GameObject obstaculoFuturo;
     private GameObject obstaculo;
     public float altura;
-    public int probMoneda;
+    private int probMoneda = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,10 +33,16 @@ public class creadorObstaculoScript : MonoBehaviour
     {
         if(tiempoInicial > tiempoMax){
                 GameObject obstaculoNuevo = Instantiate(obstaculo);
-                
+                GameObject ganarMoneda = obstaculoNuevo.transform.Find("sumarDinero").gameObject;
                 int numProb = Random.Range(0,100);
-                if(numProb>probMoneda){
-                    
+                Debug.Log(numProb);
+                Debug.Log(probMoneda);
+                if(numProb<probMoneda){
+                    Debug.Log("Deberia ponerse true");
+                    ganarMoneda.SetActive(true);
+                }else{
+                    Debug.Log("Deberia ponerse false");
+                    ganarMoneda.SetActive(false);
                 }
                 obstaculoNuevo.transform.position=transform.position+new Vector3(0,Random.Range(-altura,altura),0);
                 Destroy(obstaculoNuevo,10);
