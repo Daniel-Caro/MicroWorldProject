0bd59ba3b71fc7e90a612c286e7d7d32b3c1f6fe
davbarsal1 <72515663+davbarsal1@users.noreply.github.com>
2022-05-05 16:31:29 +0200

    Añadido videos de inicio y final

---
+++ Assets/SceneControlInicioScript.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using TMPro;
+using System.Threading.Tasks;
+
+public class SceneControlInicioScript : MonoBehaviour
+{
+    
+    GameObject notificacion;
+    GameObject videoIntro;
+    GameObject objetoDialogo; 
+    GameObject buttonNotificacion;
+    GameObject botonCorreo;
+    GameObject movilCorreo;
+    GameObject movilInteraccion;
+    GameObject backRoom;
+    VideoPlayer componenteVideoIntro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoIntro = GameObject.Find("/SceneControl/Controlador/VideoIntro");
+        componenteVideoIntro = videoIntro.GetComponent<VideoPlayer>();
+        notificacion =GameObject.Find("/SceneControl/Controlador/MovilNotificacion");
+        movilCorreo = GameObject.Find("/SceneControl/Controlador/MovilCorreo");
+        movilInteraccion = GameObject.Find("/SceneControl/Controlador/MovilInteraccion");
+        buttonNotificacion = GameObject.Find("/SceneControl/Controlador/MovilNotificacion/Canvas/BotonNotificacion");
+        botonCorreo = GameObject.Find("/SceneControl/Canvas/ProbarBoton/");
+        backRoom = GameObject.Find("/SceneControl/Controlador/BackRoom");
+        objetoDialogo = GameObject.Find("/SceneControl/Controlador/ObjetoDialogo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (( componenteVideoIntro.frame) > 0 && (componenteVideoIntro.isPlaying == false)){
+            videoIntro.SetActive(false);
+            esperarNotificacion();
+        }
+        Button compoButtonNotificacion = buttonNotificacion.GetComponent<Button>();
+        Button compoBotonCorreo = botonCorreo.GetComponent<Button>();
+        compoButtonNotificacion.onClick.AddListener(() =>{
+            notificacion.SetActive(false);
+            movilCorreo.SetActive(true);
+            botonCorreo.SetActive(true);
+        });
+        compoBotonCorreo.onClick.AddListener(()=>{
+            movilCorreo.SetActive(false);
+            botonCorreo.SetActive(false);
+            movilInteraccion.SetActive(true);
+            objetoDialogo.SetActive(true);
+        });
+
+    }
+    private async Task esperarNotificacion(){
+        notificacion.SetActive(true);
+        backRoom.SetActive(true);
+
+    }
+    
+    
+}
--- Assets/Scripts/DialogueStart.cs
+++ Assets/Scripts/DialogueStart.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using System.Threading.Tasks;
 public class DialogueStart : MonoBehaviour
 {
     [SerializeField, TextArea(4,6)] private string[] dialogueLines;
@@ -12,89 +12,397 @@ public class DialogueStart : MonoBehaviour
     [SerializeField] private GameObject decisionPanel1;
     [SerializeField] private GameObject decisionPanel2;
     [SerializeField] private GameObject decisionPanel3;
+    [SerializeField] private GameObject decisionPanel4;
+    [SerializeField] private GameObject decisionPanel5;
+    [SerializeField] private GameObject decisionPanel6;
+    [SerializeField] private GameObject decisionPanel7;
+    [SerializeField] private GameObject insertarNombre;
+    [SerializeField] private GameObject insertarEdad;
+    private GameObject videoFinal;
+    private Button enviarEdad;
+    private Text respuestaEdad;
+    private Button enviarNombre;
+    private Text respuestaNombre;
+    private Text respuestaPanel1; 
+    private Text respuestaPanel3;
     private List<int> decisiones = new List<int>();
     private bool didDialogueStart;
     private int lineIndex;
     private float timeChar;
-    
+    private bool onetime = false;
+    private GameObject movilInteraccion;
     // Start is called before the first frame update
     void Start()
     {
         StartDialogue();
+        movilInteraccion = GameObject.Find("/SceneControl/Controlador/MovilInteraccion");
+        enviarNombre = insertarNombre.transform.Find("EnviarNombre").gameObject.GetComponent<Button>();
+        enviarEdad = insertarEdad.transform.Find("EnviarEdad").gameObject.GetComponent<Button>();
+        videoFinal = GameObject.Find("/SceneControl/Controlador/VideoFinal");
     }
 
     // Update is called once per frame
     void Update()
     {
         if(decisionPanel1.activeSelf){
-           GameObject gObutton1 = decisionPanel1.transform.Find("Opcion1").gameObject;
+           GameObject gObutton1 = decisionPanel1.transform.Find("OpcionVivir").gameObject;
            Button button1 = gObutton1.GetComponent<Button>();
-           GameObject gObutton2 = decisionPanel1.transform.Find("Opcion2").gameObject;
+           GameObject gObutton2 = decisionPanel1.transform.Find("OpcionTrabajo").gameObject;
            Button button2 = gObutton2.GetComponent<Button>();
+           GameObject gObutton3 = decisionPanel1.transform.Find("OpcionEstudiar").gameObject;
+           Button button3 = gObutton3.GetComponent<Button>();
            button1.onClick.AddListener(() => {
-               decisiones.Add(1);
-               Debug.Log("Guardada opcion: "+ decisiones[0]);
-               decisionPanel1.SetActive(false);
+               if(onetime == false){
+                    decisiones.Add(1);
+                    decisionPanel1.SetActive(false);
+                    respuestaPanel1 = button1.transform.Find("Text").gameObject.GetComponent<Text>();
+                    lineIndex =3;
+                    NextDialogueLine();
+                    onetime = true;
+               }
+               
            });
            button2.onClick.AddListener(() => {
-               decisiones.Add(2);
-               Debug.Log("Guardada opcion: "+ decisiones[0]);
-               decisionPanel1.SetActive(false);
+               if(onetime == false){
+                    decisiones.Add(2);
+                    decisionPanel1.SetActive(false);
+                    respuestaPanel1 = button2.transform.Find("Text").gameObject.GetComponent<Text>();
+                    lineIndex =3;
+                    NextDialogueLine();
+                    onetime = true;
+               }
            });
+           button3.onClick.AddListener(() => {
+               if(onetime == false){
+                    decisiones.Add(3);
+                    decisionPanel1.SetActive(false);
+                    respuestaPanel1 = button3.transform.Find("Text").gameObject.GetComponent<Text>();
+                    lineIndex =3;
+                    NextDialogueLine();
+                    onetime = true;
+               }
+            });
         }else if(decisionPanel2.activeSelf){
-            GameObject gObutton3 = decisionPanel2.transform.Find("Opcion3").gameObject;
-            Button button3 = gObutton3.GetComponent<Button>();
-            GameObject gObutton4 = decisionPanel2.transform.Find("Opcion4").gameObject;
+            GameObject gObutton4 = decisionPanel2.transform.Find("OpcionNinos").gameObject;
             Button button4 = gObutton4.GetComponent<Button>();
-            button3.onClick.AddListener(() => {
-                decisiones.Add(3);
-                Debug.Log("Guardada opcion: "+ decisiones[1]);
-                decisionPanel2.SetActive(false);
-            });
+            GameObject gObutton5 = decisionPanel2.transform.Find("OpcionAnciano").gameObject;
+            Button button5 = gObutton5.GetComponent<Button>();
             button4.onClick.AddListener(() => {
-                decisiones.Add(4);
-                Debug.Log("Guardada opcion: "+ decisiones[1]);
-                decisionPanel2.SetActive(false);
+                if(onetime == true){
+                    decisiones.Add(4);
+                    Debug.Log("Guardada opcion: "+ decisiones[1]);
+                    decisionPanel2.SetActive(false);
+                    lineIndex = 5;
+                    NextDialogueLine();
+                    onetime=false;
+                }
+                
             });
-        }else if(Input.GetButtonDown("Fire1")){
-            if(text.text == dialogueLines[lineIndex]){
+            button5.onClick.AddListener(() => {
+                if(onetime == true){
+                    decisiones.Add(5);
+                    Debug.Log("Guardada opcion: "+ decisiones[1]);
+                    decisionPanel2.SetActive(false);
+                    lineIndex = 5;
+                    NextDialogueLine();
+                    onetime=false;
+                }
+            });
+        }else if(decisionPanel3.activeSelf){
+            GameObject gObutton6 = decisionPanel3.transform.Find("OpcionNinos").gameObject;
+            Button button6 = gObutton6.GetComponent<Button>();
+            GameObject gObutton7 = decisionPanel3.transform.Find("OpcionFamiliar").gameObject;
+            Button button7 = gObutton7.GetComponent<Button>();
+            button6.onClick.AddListener(() => {
+                if(onetime == false){
+                    respuestaPanel3 = button6.transform.Find("Text").gameObject.GetComponent<Text>();
+                    decisiones.Add(6);
+                    Debug.Log("Guardada opcion: "+ decisiones[1]);
+                    decisionPanel3.SetActive(false);
+                    lineIndex = 6;
+                    NextDialogueLine();
+                    onetime=true;
+                }
+                
+            });
+            button7.onClick.AddListener(() => {
+                if(onetime == false){
+                    respuestaPanel3= button7.transform.Find("Text").gameObject.GetComponent<Text>();
+                    decisiones.Add(5);
+                    Debug.Log("Guardada opcion: "+ decisiones[1]);
+                    decisionPanel3.SetActive(false);
+                    lineIndex = 6;
+                    NextDialogueLine();
+                    onetime=true;
+                }
+            });
+        }else if(decisionPanel4.activeSelf){
+           GameObject gObutton8 = decisionPanel4.transform.Find("OpcionPrincesa").gameObject;
+           Button button8 = gObutton8.GetComponent<Button>();
+           GameObject gObutton9 = decisionPanel4.transform.Find("OpcionCapitan").gameObject;
+           Button button9 = gObutton9.GetComponent<Button>();
+           GameObject gObutton10 = decisionPanel4.transform.Find("OpcionCyber").gameObject;
+           Button button10 = gObutton10.GetComponent<Button>();
+           button8.onClick.AddListener(() => {
+               if(onetime == true){
+                    decisiones.Add(8);
+                    decisionPanel4.SetActive(false);
+                    lineIndex =9;
+                    NextDialogueLine();
+                    onetime = false;
+               }
+               
+           });
+           button9.onClick.AddListener(() => {
+               if(onetime == true){
+                    decisiones.Add(9);
+                    decisionPanel4.SetActive(false);
+                    lineIndex =9;
+                    NextDialogueLine();
+                    onetime = false;
+               }
+           });
+           button10.onClick.AddListener(() => {
+               if(onetime == true){
+                    decisiones.Add(10);
+                    decisionPanel4.SetActive(false);
+                    lineIndex =9;
+                    NextDialogueLine();
+                    onetime = false;
+               }
+            });
+        }else if(decisionPanel5.activeSelf){
+           GameObject gObutton11 = decisionPanel5.transform.Find("OpcionAntorcha").gameObject;
+           Button button11 = gObutton11.GetComponent<Button>();
+           GameObject gObutton12 = decisionPanel5.transform.Find("OpcionPiedra").gameObject;
+           Button button12 = gObutton12.GetComponent<Button>();
+           GameObject gObutton13 = decisionPanel5.transform.Find("OpcionPalo").gameObject;
+           Button button13 = gObutton13.GetComponent<Button>();
+           button11.onClick.AddListener(() => {
+               if(onetime == false){
+                    decisiones.Add(11);
+                    decisionPanel5.SetActive(false);
+                    lineIndex =11;
+                    NextDialogueLine();
+                    onetime = true;
+               }
+               
+           });
+           button12.onClick.AddListener(() => {
+               if(onetime == false){
+                    decisiones.Add(12);
+                    decisionPanel5.SetActive(false);
+                    lineIndex =11;
+                    NextDialogueLine();
+                    onetime = true;
+               }
+           });
+           button13.onClick.AddListener(() => {
+               if(onetime == false){
+                    decisiones.Add(13);
+                    decisionPanel5.SetActive(false);
+                    lineIndex =11;
+                    NextDialogueLine();
+                    onetime = true;
+               }
+            });
+        }else if(decisionPanel6.activeSelf){
+           GameObject gObutton14 = decisionPanel6.transform.Find("OpcionJack").gameObject;
+           Button button14 = gObutton14.GetComponent<Button>();
+           GameObject gObutton15 = decisionPanel6.transform.Find("OpcionHeleen").gameObject;
+           Button button15 = gObutton15.GetComponent<Button>();
+           GameObject gObutton16 = decisionPanel6.transform.Find("OpcionElon").gameObject;
+           Button button16 = gObutton16.GetComponent<Button>();
+           button14.onClick.AddListener(() => {
+               if(onetime == true){
+                    decisiones.Add(14);
+                    decisionPanel6.SetActive(false);
+                    lineIndex =12;
+                    NextDialogueLine();
+                    onetime = false;
+               }
+               
+           });
+           button15.onClick.AddListener(() => {
+               if(onetime == true){
+                    decisiones.Add(15);
+                    decisionPanel6.SetActive(false);
+                    lineIndex =12;
+                    NextDialogueLine();
+                    onetime = false;
+               }
+           });
+           button16.onClick.AddListener(() => {
+               if(onetime == true){
+                    decisiones.Add(16);
+                    decisionPanel6.SetActive(false);
+                    lineIndex =12;
+                    NextDialogueLine();
+                    onetime = false;
+               }
+            });
+        }else if(decisionPanel7.activeSelf){
+           GameObject gObutton17 = decisionPanel7.transform.Find("OpcionPiratas").gameObject;
+           Button button17 = gObutton17.GetComponent<Button>();
+           GameObject gObutton18 = decisionPanel7.transform.Find("OpcionPiratas").gameObject;
+           Button button18 = gObutton18.GetComponent<Button>();
+           GameObject gObutton19 = decisionPanel7.transform.Find("OpcionFuturo").gameObject;
+           Button button19 = gObutton19.GetComponent<Button>();
+           button17.onClick.AddListener(() => {
+               if(onetime == false){
+                    decisionPanel7.SetActive(false);
+                    lineIndex = 13;
+                    NextDialogueLine();
+                    onetime=true;
+               }
+               
+           });
+           button18.onClick.AddListener(() => {
+               if(onetime == false){
+                    decisionPanel7.SetActive(false);
+                    lineIndex =13;
+                    NextDialogueLine();
+                    onetime = true;
+               }
+           });
+           button19.onClick.AddListener(() => {
+               if(onetime == false){
+                    decisionPanel7.SetActive(false);
+                    lineIndex =13;
+                    NextDialogueLine();
+                    onetime = true;
+               }
+            });
+        }else if(insertarNombre.activeSelf && lineIndex == 0){
+            enviarNombre.onClick.AddListener(() => {
+                if(onetime == false){
+                    respuestaNombre = insertarNombre.transform.Find("Text").GetComponent<Text>();
+                    insertarNombre.SetActive(false);
+                    lineIndex = 1;
+                    NextDialogueLine();
+                    onetime = true;
+                }
+                
+            }); 
+        }else if(insertarEdad.activeSelf && lineIndex == 1){
+            enviarEdad.onClick.AddListener(() => {
+                
+                if(onetime == true){
+                    respuestaEdad = insertarEdad.transform.Find("Text").GetComponent<Text>();
+                    insertarEdad.SetActive(false);
+                    lineIndex = 2;
+                    Debug.Log(onetime);
+                    NextDialogueLine();
+                    onetime = false;
+                }
+                
+            }); 
+        }else if((Input.GetButtonDown("Fire1")) && (lineIndex == 3 || lineIndex == 6 || lineIndex == 7 || lineIndex == 9 || lineIndex == 12)){
+                lineIndex++;
                 NextDialogueLine();
-            }
+        }else if((Input.GetButtonDown("Fire1")) &&(lineIndex == 0 || lineIndex == 1 || lineIndex == 2 || lineIndex == 4 || lineIndex == 5 || lineIndex == 8 || lineIndex ==10 || lineIndex == 11)){
+            Debug.Log("Primera linea");
+            StartCoroutine(ShowLine(true));
+                    
+
         }
     }
     private void StartDialogue(){
         didDialogueStart = true;
         panel.SetActive(true);
         lineIndex = 0;
-        StartCoroutine(ShowLine());
+        StartCoroutine(ShowLine(false));
     }
     private void NextDialogueLine(){
-        lineIndex++;
-        
+     
+        Debug.Log("Hola caracola:"+ lineIndex);
+        panel.SetActive(true);
         if(lineIndex < dialogueLines.Length){
-            StartCoroutine(ShowLine());
+            StartCoroutine(ShowLine(false));
             
         }else {
+            Debug.Log("Hola pesicola:"+ lineIndex);
             panel.SetActive(false);
         }
     }
-    private IEnumerator ShowLine(){
+    private IEnumerator ShowLine(bool pulsarPanel){
         text.text = string.Empty;
-        foreach(char ch in dialogueLines[lineIndex]){
-            text.text += ch;
-            yield return new WaitForSeconds(timeChar);
-        }controladorPanel(lineIndex);
+        if(lineIndex == 1){
+            
+            dialogueLines[1] = "Genial "+ respuestaNombre.text +", que edad tienes?";
+            
+        }else if(lineIndex == 3){
+            dialogueLines[3] = "Genial! Siempre esta bien "+ respuestaPanel1.text+". Ahora vamos con preguntas mas complejas.";
+        }else if(lineIndex == 6){
+            dialogueLines[6] = "De acuerdo, has elegido salvar a "+ respuestaPanel3.text + ", justo lo que esperabamos de ti "+respuestaNombre.text;
+        }
+        if(pulsarPanel == false){
+            Debug.Log(dialogueLines[3]);
+            foreach(char ch in dialogueLines[lineIndex]){
+                text.text += ch;
+                yield return new WaitForSeconds(timeChar);
+            }if(lineIndex == 13){
+                controladorPanel(lineIndex);
+            }
+        }else{
+            controladorPanel(lineIndex);
+        }
     }
-    private void controladorPanel(int lineIndex){
-        if(lineIndex == 3){
+    private async Task controladorPanel(int lineIndex){
+        if(lineIndex == 13){
+            panel.SetActive(false);
+            movilInteraccion.SetActive(false);
+            videoFinal.SetActive(true);
+
+        }
+        else if(lineIndex == 12){
+      
+            panel.SetActive(false);
+            decisionPanel7.SetActive(true);
+        }
+        else if(lineIndex == 11){
+       
+            panel.SetActive(false);
+            decisionPanel6.SetActive(true);
+        }
+        else if(lineIndex == 10){
+     
+            panel.SetActive(false);
+            decisionPanel5.SetActive(true);
+        }
+        else if(lineIndex == 8){
+           
+            panel.SetActive(false);
+            decisionPanel4.SetActive(true);
+        }
+        else if(lineIndex == 5){
+           
+            panel.SetActive(false);
+            decisionPanel3.SetActive(true);
+        }
+        else if(lineIndex == 4){
+         
+            panel.SetActive(false);
             decisionPanel2.SetActive(true);
-        }else if(lineIndex == 2){
+        }
+        else if(lineIndex == 2){
+    
+            panel.SetActive(false);
             decisionPanel1.SetActive(true);
         }else if(lineIndex == 1){
+           
+            panel.SetActive(false);
+            insertarEdad.SetActive(true);
             
         }else if(lineIndex == 0){
             
+            panel.SetActive(false);
+            insertarNombre.SetActive(true);
+           
+           
+            
         }
     }
-    
+  
+
 }
